7cd05fe2a1240d82dda6bd2e2aa3e6e868aabcdd
Wellington Junior <wellingtonjunior@outlook.com>
2023-05-01 17:13:01 -0300

    Create the integration file

--- backend/integration-test/integration_test.go
+++ backend/integration-test/integration_test.go
@@ -1,1 +1,57 @@
 package integration_test
+
+import (
+	"log"
+	"net/http"
+	"os"
+	"testing"
+	"time"
+
+	. "github.com/Eun/go-hit"
+)
+
+const (
+	// Attempts connection
+	host       = "localhost:8080"
+	healthPath = "http://" + host + "/healthz"
+	attempts   = 20
+
+	// HTTP REST
+	basePath = "http://" + host + "/v1"
+
+	// Kafka
+	kafkaHost     = "kafka:9092"
+	consumerTopic = "consumer_topic"
+	producerTopic = "producer_topic"
+)
+
+func TestMain(t *testing.M) {
+	err := healthCheck(attempts)
+	if err != nil {
+		log.Fatalf("Integration tests: host %s is not available: %s", host, err)
+	}
+
+	log.Printf("Integration tests: host %s is available", host)
+
+	code := t.Run()
+	os.Exit(code)
+}
+
+func healthCheck(attempts int) error {
+	var err error
+
+	for attempts > 0 {
+		err = Do(Get(healthPath), Expect().Status().Equal(http.StatusOK))
+		if err == nil {
+			return nil
+		}
+
+		log.Printf("Integration tests: url %s is not available, attempts left: %d", healthPath, attempts)
+
+		time.Sleep(time.Second)
+
+		attempts--
+	}
+
+	return err
+}
