9aa4050efb1695b754396bed342c113b7011e5a4
Wellington Junior <wellingtonjunior@outlook.com>
2023-07-31 14:36:16 -0300

    Tests : Add the useCase Tests

--- backend/internal/usecase/vault.go
+++ backend/internal/usecase/vault.go
@@ -19,7 +19,6 @@ func NewVaultUseCase(vRepo VaultRepoInterface, wRepo WalletRepoInterface) *Vault
 }
 
 func (uc *VaultUseCase) Create(data entity.Vault) (entity.Vault, error) {
-	fmt.Print(data.Wallet)
 	wallet, err := uc.wRepo.CreateWalletWithKey(data.Wallet)
 	if err != nil {
 		return entity.Vault{}, fmt.Errorf("VaultUseCase - Create - uc.repo.CreateWalletWithKey(dist): %w", err)
--- backend/internal/usecase/vault_test.go
+++ backend/internal/usecase/vault_test.go
@@ -109,3 +109,143 @@ func TestVaultUseCaseList(t *testing.T) {
 		})
 	}
 }
+
+func TestVaultUseCaseGetById(t *testing.T) {
+	u, vr, _, _ := vault(t)
+
+	vault1 := entity.Vault{
+		Id:   1,
+		Name: "Treasury",
+		Wallet: entity.Wallet{
+			Type: entity.SponsorType,
+		},
+		VaultCategory: entity.VaultCategory{
+			Id:   1,
+			Name: "Some Category",
+		},
+	}
+
+	vault2 := entity.Vault{
+		Id:   2,
+		Name: "Safe",
+		Wallet: entity.Wallet{
+			Type: entity.IssuerType,
+		},
+		VaultCategory: entity.VaultCategory{
+			Id:   2,
+			Name: "Another Category",
+		},
+	}
+
+	tests := []testVault{
+		{
+			name: "get - vault 1- Sucess",
+			req:  1,
+			mock: func() {
+				vr.EXPECT().GetVaultById(1).Return(vault1, nil)
+			},
+			res: vault1,
+			err: nil,
+		},
+		{
+			name: "get - vault 2 - Sucess",
+			req:  2,
+			mock: func() {
+				vr.EXPECT().GetVaultById(2).Return(vault2, nil)
+			},
+			res: vault2,
+			err: nil,
+		},
+		{
+			name: "get - vault 3 - Not Found",
+			req:  3,
+			mock: func() {
+				vr.EXPECT().GetVaultById(3).Return(entity.Vault{}, vaultDbError)
+			},
+			res: entity.Vault{},
+			err: vaultDbError,
+		},
+	}
+	for _, tc := range tests {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			tc.mock()
+
+			res, err := u.GetById(tc.req.(int))
+
+			require.EqualValues(t, tc.res, res)
+			if tc.err == nil {
+				require.EqualValues(t, err, tc.err)
+			} else {
+				require.ErrorContains(t, err, dbError.Error())
+			}
+		})
+	}
+}
+
+func TestVaultUseCaseCreate(t *testing.T) {
+	u, vr, wr, _ := vault(t)
+
+	vault := entity.Vault{
+		Id:   1,
+		Name: "Treasury",
+		Wallet: entity.Wallet{
+			Type: entity.SponsorType,
+		},
+		VaultCategory: entity.VaultCategory{
+			Id:   1,
+			Name: "Some Category",
+		},
+	}
+
+	tests := []testVault{
+		{
+			name: "create - vault - Sucess",
+			req:  vault,
+			mock: func() {
+				wr.EXPECT().CreateWalletWithKey(vault.Wallet).Return(vault.Wallet, nil)
+				vr.EXPECT().CreateVault(vault).Return(vault, nil)
+			},
+			res: vault,
+			err: nil,
+		},
+		{
+			name: "create - vault - Database Error",
+			req:  vault,
+			mock: func() {
+				wr.EXPECT().CreateWalletWithKey(vault.Wallet).Return(vault.Wallet, nil)
+				vr.EXPECT().CreateVault(vault).Return(entity.Vault{}, vaultDbError)
+			},
+			res: entity.Vault{},
+			err: vaultDbError,
+		},
+		{
+			name: "create - vault - Wallet Error",
+			req:  vault,
+			mock: func() {
+				wr.EXPECT().CreateWalletWithKey(vault.Wallet).Return(entity.Wallet{}, walletDbError)
+			},
+			res: entity.Vault{},
+			err: walletDbError,
+		},
+	}
+
+	for _, tc := range tests {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			tc.mock()
+
+			res, err := u.Create(tc.req.(entity.Vault))
+
+			require.EqualValues(t, tc.res, res)
+			if tc.err == nil {
+				require.EqualValues(t, err, tc.err)
+			} else {
+				require.ErrorContains(t, err, dbError.Error())
+			}
+		})
+	}
+}
+
+func TestsVaultUseCaseUpdateVault(t *testing.T) {
+}
