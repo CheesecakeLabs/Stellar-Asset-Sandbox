fe514460dfd2e56d657f10f0b54ec5c9bde17953
Wellington Junior <wellingtonjunior@outlook.com>
2023-08-08 15:29:16 -0300

    ✅  Add Generate TOML FIle test

--- backend/internal/usecase/assets_test.go
+++ backend/internal/usecase/assets_test.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"testing"
 
+	"github.com/CheesecakeLabs/token-factory-v2/backend/config"
 	"github.com/CheesecakeLabs/token-factory-v2/backend/internal/entity"
 	"github.com/CheesecakeLabs/token-factory-v2/backend/internal/usecase"
 	"github.com/CheesecakeLabs/token-factory-v2/backend/internal/usecase/mocks"
@@ -25,7 +26,7 @@ type testAsset struct {
 	err  error
 }
 
-func asset(t *testing.T) (*usecase.AssetUseCase, *mocks.MockAssetRepoInterface, *mocks.MockWalletRepoInterface) {
+func asset(t *testing.T) (*usecase.AssetUseCase, *mocks.MockAssetRepoInterface, *mocks.MockWalletRepoInterface, *mocks.MockTomlInterface) {
 	t.Helper()
 
 	mockCtl := gomock.NewController(t)
@@ -33,13 +34,14 @@ func asset(t *testing.T) (*usecase.AssetUseCase, *mocks.MockAssetRepoInterface, 
 
 	rw := mocks.NewMockWalletRepoInterface(mockCtl)
 	ra := mocks.NewMockAssetRepoInterface(mockCtl)
-	u := usecase.NewAssetUseCase(ra, rw)
+	tg := mocks.NewMockTomlInterface(mockCtl)
+	u := usecase.NewAssetUseCase(ra, rw, tg, config.Horizon{})
 
-	return u, ra, rw
+	return u, ra, rw, tg
 }
 
 func TestAssetUseCaseCreate(t *testing.T) {
-	u, ra, rw := asset(t)
+	u, ra, rw, tg := asset(t)
 
 	req := entity.Asset{
 		Code: "ABC",
@@ -197,6 +199,123 @@ func TestAssetUseCaseCreate(t *testing.T) {
 				require.EqualValues(t, err, tc.err)
 			} else {
 				require.ErrorContains(t, err, tc.err.Error())
+			}
+		})
+	}
+
+	assetID := 123
+	mockAsset := entity.Asset{
+		Id:   assetID,
+		Code: "ABC",
+	}
+
+	mockAssets := []entity.Asset{
+		mockAsset,
+	}
+
+	mockToml := "mock toml data"
+	mockReq := entity.TomlData{
+		Version: "2.0.0",
+	}
+
+	tests = []testAsset{
+		// Test for GetById
+		{
+			name: "get by id - success",
+			req:  assetID,
+			mock: func() {
+				ra.EXPECT().GetAssetById(assetID).Return(mockAsset, nil)
+			},
+			res: mockAsset,
+			err: nil,
+		},
+		// Add other test cases for GetById...
+		{
+			name: "get by id - database error",
+			req:  assetID,
+			mock: func() {
+				ra.EXPECT().GetAssetById(assetID).Return(entity.Asset{}, assetDbError)
+			},
+			res: entity.Asset{},
+			err: assetDbError,
+		},
+		// Test for GetAll
+		{
+			name: "get all - success",
+			mock: func() {
+				ra.EXPECT().GetAssets().Return(mockAssets, nil)
+			},
+			res: mockAssets,
+			err: nil,
+		},
+		{
+			name: "get all - database error",
+			mock: func() {
+				ra.EXPECT().GetAssets().Return([]entity.Asset{}, assetDbError)
+			},
+			res: []entity.Asset{},
+			err: assetDbError,
+		},
+		{
+			name: "create toml - success",
+			req:  mockReq,
+			mock: func() {
+				mockCfg := config.Horizon{} // Mock configuration
+				mockTRepo := tg
+				mockTRepo.EXPECT().GenerateToml(mockReq, mockCfg).Return(mockToml, nil)
+			},
+			res: mockToml,
+			err: nil,
+		},
+		{
+			name: "create toml - error",
+			req:  mockReq,
+			mock: func() {
+				mockCfg := config.Horizon{} // Mock configuration
+				mockTRepo := tg
+				mockTRepo.EXPECT().GenerateToml(mockReq, mockCfg).Return("", errors.New("error"))
+			},
+			res: "",
+			err: errors.New("error"),
+		},
+	}
+
+	for _, tc := range tests {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			tc.mock()
+
+			// Test GetById
+			if assetID, ok := tc.req.(string); ok {
+				res, err := u.GetById(assetID)
+				require.EqualValues(t, tc.res, res)
+				if tc.err == nil {
+					require.EqualValues(t, err, tc.err)
+				} else {
+					require.ErrorContains(t, err, tc.err.Error())
+				}
+			}
+
+			// Test GetAll
+			if _, ok := tc.req.(bool); ok {
+				res, err := u.GetAll()
+				require.EqualValues(t, tc.res, res)
+				if tc.err == nil {
+					require.EqualValues(t, err, tc.err)
+				} else {
+					require.ErrorContains(t, err, tc.err.Error())
+				}
+			}
+
+			// Test CreateToml
+			if req, ok := tc.req.(entity.TomlData); ok {
+				res, err := u.CreateToml(req)
+				require.EqualValues(t, tc.res, res)
+				if tc.err == nil {
+					require.EqualValues(t, err, tc.err)
+				} else {
+					require.ErrorContains(t, err, tc.err.Error())
+				}
 			}
 		})
 	}
