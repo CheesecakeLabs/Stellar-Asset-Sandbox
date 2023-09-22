ccd5023b942c56a4c2a66d07408b9a0cf58c88b0
Lucas Magnus <lucasmagnusdasilva@gmail.com>
2023-09-22 16:16:56 -0300

    fix routes

--- frontend/src/app/auth/hocs/require-auth.tsx
+++ frontend/src/app/auth/hocs/require-auth.tsx
@@ -2,12 +2,15 @@ import { Navigate, useLocation } from 'react-router-dom'
 
 import { useAuth } from 'hooks/useAuth'
 
+
 const RequireAuth = ({ children }: { children: JSX.Element }): JSX.Element => {
   const { isAuthenticated } = useAuth()
   const location = useLocation()
 
   if (!isAuthenticated) {
-    return <Navigate to="/login" state={{ from: location }} replace />
+    return (
+      <Navigate to={`/sandbox/v2/login`} state={{ from: location }} replace />
+    )
   }
 
   return children
