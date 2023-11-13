d80f2d9d932e40b77b8fb3bff800ed0a0c4cc0b0
Wellington Junior <wellingtonjunior@outlook.com>
2023-11-13 16:38:22 -0300

    Add GetPaginatedAssets method to MockAssetRepoInterface

--- backend/internal/usecase/mocks/mocks.go
+++ backend/internal/usecase/mocks/mocks.go
@@ -461,6 +461,21 @@ func (mr *MockAssetRepoInterfaceMockRecorder) GetAssets() *gomock.Call {
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "GetAssets", reflect.TypeOf((*MockAssetRepoInterface)(nil).GetAssets))
 }
 
+// GetPaginatedAssets mocks base method.
+func (m *MockAssetRepoInterface) GetPaginatedAssets(arg0, arg1 int) ([]entity.Asset, error) {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "GetPaginatedAssets", arg0, arg1)
+	ret0, _ := ret[0].([]entity.Asset)
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
+}
+
+// GetPaginatedAssets indicates an expected call of GetPaginatedAssets.
+func (mr *MockAssetRepoInterfaceMockRecorder) GetPaginatedAssets(arg0, arg1 interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "GetPaginatedAssets", reflect.TypeOf((*MockAssetRepoInterface)(nil).GetPaginatedAssets), arg0, arg1)
+}
+
 // StoreAssetImage mocks base method.
 func (m *MockAssetRepoInterface) StoreAssetImage(arg0 string, arg1 []byte) error {
 	m.ctrl.T.Helper()
