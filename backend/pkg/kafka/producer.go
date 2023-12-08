2b3b1037b08fd6927d52a4927f16f5ba2c96b99d
Lucas Magnus <lucasmagnusdasilva@gmail.com>
2023-12-08 11:30:30 -0300

    log contract submit producer

--- backend/pkg/kafka/producer.go
+++ backend/pkg/kafka/producer.go
@@ -17,13 +17,21 @@ func (p *Producer) Produce(key string, value interface{}) error {
 	if err != nil {
 		return fmt.Errorf("Producer - Produce - json.Marshal: %v", err)
 	}
+
 	err = p.exec.Produce(&kafka.Message{
 		Key:            []byte(key),
 		TopicPartition: kafka.TopicPartition{Topic: &p.Topic, Partition: kafka.PartitionAny},
 		Value:          valueMarshalled,
 	}, nil)
+
 	if err != nil {
-		return fmt.Errorf("Producer - Produce - p.exec.Produce: %v", err)
+		//return fmt.Errorf("Producer - Produce - p.exec.Produce: %v", err)
+		str1 := []byte(key)
+		str2 := key
+		str3 := value
+		str4 := valueMarshalled
+		return fmt.Errorf(fmt.Sprintf("%s%s%s%s", str1,
+			str2, str3, str4), err)
 	}
 
 	return nil
