16d4efda4598621a95377df4774dfe9660352801
Wellington Junior <wellingtonjunior@outlook.com>
2023-12-18 10:12:38 -0300

    Add the edge case, if image is null

--- backend/internal/usecase/repo/asset_postgres.go
+++ backend/internal/usecase/repo/asset_postgres.go
@@ -36,23 +36,24 @@ func (r AssetRepo) GetAsset(id int) (entity.Asset, error) {
 
 func (r AssetRepo) GetAssets() ([]entity.Asset, error) {
 	query := `
-		SELECT
-			a.id AS asset_id, a.name AS asset_name, a.asset_type, a.code AS code, a.image, a.contract_id,
-			d.id AS distributor_id, d.type AS distributor_type, d.funded AS distributor_funded,
-			dk.id AS distributor_key_id, dk.public_key AS distributor_key_public_key, dk.weight AS distributor_key_weight,
-			i.id AS issuer_id, i.type AS issuer_type, i.funded AS issuer_funded,
-			ik.id AS issuer_key_id, ik.public_key AS issuer_key_public_key, ik.weight AS issuer_key_weight
-		FROM asset a
-		JOIN wallet d ON a.distributor_id = d.id
-		JOIN key dk ON d.id = dk.wallet_id
-		JOIN wallet i ON a.issuer_id = i.id
-		JOIN key ik ON i.id = ik.wallet_id
-		ORDER BY a.name;
-	`
+        SELECT
+            a.id AS asset_id, a.name AS asset_name, a.asset_type, a.code AS code, 
+            COALESCE(a.image, '') AS image, a.contract_id,
+            d.id AS distributor_id, d.type AS distributor_type, d.funded AS distributor_funded,
+            dk.id AS distributor_key_id, dk.public_key AS distributor_key_public_key, dk.weight AS distributor_key_weight,
+            i.id AS issuer_id, i.type AS issuer_type, i.funded AS issuer_funded,
+            ik.id AS issuer_key_id, ik.public_key AS issuer_key_public_key, ik.weight AS issuer_key_weight
+        FROM asset a
+        JOIN wallet d ON a.distributor_id = d.id
+        JOIN key dk ON d.id = dk.wallet_id
+        JOIN wallet i ON a.issuer_id = i.id
+        JOIN key ik ON i.id = ik.wallet_id
+        ORDER BY a.name;
+    `
 
 	rows, err := r.Db.Query(query)
 	if err != nil {
-		return nil, fmt.Errorf("AssetRepo - GetAllAssets - Query: %w", err)
+		return nil, fmt.Errorf("AssetRepo - GetAssets - Query: %w", err)
 	}
 	defer rows.Close()
 
@@ -62,16 +63,23 @@ func (r AssetRepo) GetAssets() ([]entity.Asset, error) {
 		var asset entity.Asset
 		var distributor entity.Wallet
 		var issuer entity.Wallet
+		var image sql.NullString
 
 		err := rows.Scan(
-			&asset.Id, &asset.Name, &asset.AssetType, &asset.Code, &asset.Image, &asset.ContractId,
+			&asset.Id, &asset.Name, &asset.AssetType, &asset.Code, &image,
 			&distributor.Id, &distributor.Type, &distributor.Funded,
 			&distributor.Key.Id, &distributor.Key.PublicKey, &distributor.Key.Weight,
 			&issuer.Id, &issuer.Type, &issuer.Funded,
 			&issuer.Key.Id, &issuer.Key.PublicKey, &issuer.Key.Weight,
 		)
 		if err != nil {
-			return nil, fmt.Errorf("AssetRepo - GetAllAssets - row.Scan: %w", err)
+			return nil, fmt.Errorf("AssetRepo - GetAssets - row.Scan: %w", err)
+		}
+
+		if image.Valid {
+			asset.Image = image.String
+		} else {
+			asset.Image = ""
 		}
 
 		asset.Distributor = distributor
@@ -85,28 +93,30 @@ func (r AssetRepo) GetAssets() ([]entity.Asset, error) {
 
 func (r AssetRepo) GetAssetByCode(code string) (entity.Asset, error) {
 	query := `
-		SELECT
-			a.id AS asset_id, a.name AS asset_name, a.asset_type,a.code as asset_code, a.image, a.contract_id,
-			d.id AS distributor_id, d.type AS distributor_type, d.funded AS distributor_funded,
-			dk.id AS distributor_key_id, dk.public_key AS distributor_key_public_key, dk.weight AS distributor_key_weight,
-			i.id AS issuer_id, i.type AS issuer_type, i.funded AS issuer_funded,
-			ik.id AS issuer_key_id, ik.public_key AS issuer_key_public_key, ik.weight AS issuer_key_weight
-		FROM asset a
-		JOIN wallet d ON a.distributor_id = d.id
-		JOIN key dk ON d.id = dk.wallet_id
-		JOIN wallet i ON a.issuer_id = i.id
-		JOIN key ik ON i.id = ik.wallet_id
-		WHERE a.code = $1;
-	`
+        SELECT
+            a.id AS asset_id, a.name AS asset_name, a.asset_type, a.code AS code, 
+            COALESCE(a.image, '') AS image, a.contract_id,
+            d.id AS distributor_id, d.type AS distributor_type, d.funded AS distributor_funded,
+            dk.id AS distributor_key_id, dk.public_key AS distributor_key_public_key, dk.weight AS distributor_key_weight,
+            i.id AS issuer_id, i.type AS issuer_type, i.funded AS issuer_funded,
+            ik.id AS issuer_key_id, ik.public_key AS issuer_key_public_key, ik.weight AS issuer_key_weight
+        FROM asset a
+        JOIN wallet d ON a.distributor_id = d.id
+        JOIN key dk ON d.id = dk.wallet_id
+        JOIN wallet i ON a.issuer_id = i.id
+        JOIN key ik ON i.id = ik.wallet_id
+        WHERE a.code = $1;
+    `
 
 	row := r.Db.QueryRow(query, code)
 
 	var asset entity.Asset
 	var distributor entity.Wallet
 	var issuer entity.Wallet
+	var image sql.NullString
 
 	err := row.Scan(
-		&asset.Id, &asset.Name, &asset.AssetType, &asset.Code, &asset.Image, &asset.ContractId,
+		&asset.Id, &asset.Name, &asset.AssetType, &asset.Code, &image,
 		&distributor.Id, &distributor.Type, &distributor.Funded,
 		&distributor.Key.Id, &distributor.Key.PublicKey, &distributor.Key.Weight,
 		&issuer.Id, &issuer.Type, &issuer.Funded,
@@ -119,47 +129,44 @@ func (r AssetRepo) GetAssetByCode(code string) (entity.Asset, error) {
 		return entity.Asset{}, fmt.Errorf("AssetRepo - GetAssetByCode - row.Scan: %w", err)
 	}
 
+	if image.Valid {
+		asset.Image = image.String
+	} else {
+		asset.Image = ""
+	}
+
 	asset.Distributor = distributor
 	asset.Issuer = issuer
 
 	return asset, nil
 }
 
-func (r AssetRepo) CreateAsset(data entity.Asset) (entity.Asset, error) {
-	res := data
-	stmt := `INSERT INTO Asset (code, issuer_id, distributor_id, name, asset_type, image ) VALUES ($1, $2, $3,$4, $5, $6) RETURNING id;`
-	err := r.Db.QueryRow(stmt, data.Code, data.Issuer.Id, data.Distributor.Id, data.Name, data.AssetType, data.Image).Scan(&res.Id)
-	if err != nil {
-		return entity.Asset{}, fmt.Errorf("AssetRepo - CreateAsset - db.QueryRow: %w", err)
-	}
-
-	return res, nil
-}
-
 func (r AssetRepo) GetAssetById(id string) (entity.Asset, error) {
 	query := `
-		SELECT
-			a.id AS asset_id, a.name AS asset_name, a.asset_type, a.code as asset_code, a.image, a.contract_id,
-			d.id AS distributor_id, d.type AS distributor_type, d.funded AS distributor_funded,
-			dk.id AS distributor_key_id, dk.public_key AS distributor_key_public_key, dk.weight AS distributor_key_weight,
-			i.id AS issuer_id, i.type AS issuer_type, i.funded AS issuer_funded,
-			ik.id AS issuer_key_id, ik.public_key AS issuer_key_public_key, ik.weight AS issuer_key_weight
-		FROM asset a
-		JOIN wallet d ON a.distributor_id = d.id
-		JOIN key dk ON d.id = dk.wallet_id
-		JOIN wallet i ON a.issuer_id = i.id
-		JOIN key ik ON i.id = ik.wallet_id
-		WHERE a.id = $1;
-	`
+        SELECT
+            a.id AS asset_id, a.name AS asset_name, a.asset_type, a.code AS code, 
+            COALESCE(a.image, '') AS image, a.contract_id,
+            d.id AS distributor_id, d.type AS distributor_type, d.funded AS distributor_funded,
+            dk.id AS distributor_key_id, dk.public_key AS distributor_key_public_key, dk.weight AS distributor_key_weight,
+            i.id AS issuer_id, i.type AS issuer_type, i.funded AS issuer_funded,
+            ik.id AS issuer_key_id, ik.public_key AS issuer_key_public_key, ik.weight AS issuer_key_weight
+        FROM asset a
+        JOIN wallet d ON a.distributor_id = d.id
+        JOIN key dk ON d.id = dk.wallet_id
+        JOIN wallet i ON a.issuer_id = i.id
+        JOIN key ik ON i.id = ik.wallet_id
+        WHERE a.id = $1;
+    `
 
 	row := r.Db.QueryRow(query, id)
 
 	var asset entity.Asset
 	var distributor entity.Wallet
 	var issuer entity.Wallet
+	var image sql.NullString
 
 	err := row.Scan(
-		&asset.Id, &asset.Name, &asset.AssetType, &asset.Code, &asset.Image, &asset.ContractId,
+		&asset.Id, &asset.Name, &asset.AssetType, &asset.Code, &image,
 		&distributor.Id, &distributor.Type, &distributor.Funded,
 		&distributor.Key.Id, &distributor.Key.PublicKey, &distributor.Key.Weight,
 		&issuer.Id, &issuer.Type, &issuer.Funded,
@@ -172,10 +179,91 @@ func (r AssetRepo) GetAssetById(id string) (entity.Asset, error) {
 		return entity.Asset{}, fmt.Errorf("AssetRepo - GetAssetById - row.Scan: %w", err)
 	}
 
+	if image.Valid {
+		asset.Image = image.String
+	} else {
+		asset.Image = ""
+	}
+
 	asset.Distributor = distributor
 	asset.Issuer = issuer
 
 	return asset, nil
+}
+
+func (r AssetRepo) GetPaginatedAssets(page int, limit int) ([]entity.Asset, int, error) {
+	// Calculate the offset
+	offset := (page - 1) * limit
+
+	// Query to count the total number of assets
+	countQuery := `
+        SELECT COUNT(*)
+        FROM asset;
+    `
+	var totalAssets int
+	err := r.Db.QueryRow(countQuery).Scan(&totalAssets)
+	if err != nil {
+		return nil, 0, fmt.Errorf("AssetRepo - GetPaginated - Count Query: %w", err)
+	}
+
+	// Calculate total pages
+	totalPages := (totalAssets + limit - 1) / limit
+
+	// Query to fetch paginated assets
+	query := `
+        SELECT
+            a.id AS asset_id, a.name AS asset_name, a.asset_type, a.code AS code, 
+            COALESCE(a.image, '') AS image, a.contract_id,
+            d.id AS distributor_id, d.type AS distributor_type, d.funded AS distributor_funded,
+            dk.id AS distributor_key_id, dk.public_key AS distributor_key_public_key, dk.weight AS distributor_key_weight,
+            i.id AS issuer_id, i.type AS issuer_type, i.funded AS issuer_funded,
+            ik.id AS issuer_key_id, ik.public_key AS issuer_key_public_key, ik.weight AS issuer_key_weight
+        FROM asset a
+        JOIN wallet d ON a.distributor_id = d.id
+        JOIN key dk ON d.id = dk.wallet_id
+        JOIN wallet i ON a.issuer_id = i.id
+        JOIN key ik ON i.id = ik.wallet_id
+        ORDER BY a.name
+        LIMIT $1 OFFSET $2;
+    `
+
+	rows, err := r.Db.Query(query, limit, offset)
+	if err != nil {
+		return nil, 0, fmt.Errorf("AssetRepo - GetPaginated - Query: %w", err)
+	}
+	defer rows.Close()
+
+	var assets []entity.Asset
+	for rows.Next() {
+		var asset entity.Asset
+		var distributor entity.Wallet
+		var issuer entity.Wallet
+		var image sql.NullString
+
+		err := rows.Scan(
+			&asset.Id, &asset.Name, &asset.AssetType, &asset.Code, &image,
+			&distributor.Id, &distributor.Type, &distributor.Funded,
+			&distributor.Key.Id, &distributor.Key.PublicKey, &distributor.Key.Weight,
+			&issuer.Id, &issuer.Type, &issuer.Funded,
+			&issuer.Key.Id, &issuer.Key.PublicKey, &issuer.Key.Weight,
+		)
+		if err != nil {
+			return nil, 0, fmt.Errorf("AssetRepo - GetPaginated - row.Scan: %w", err)
+		}
+
+		if image.Valid {
+			asset.Image = image.String
+		} else {
+			asset.Image = ""
+		}
+
+		asset.Distributor = distributor
+		asset.Issuer = issuer
+
+		assets = append(assets, asset)
+	}
+
+	return assets, totalPages, nil
 }
 
 func (r AssetRepo) StoreAssetImage(assetId string, image string) error {
@@ -226,69 +314,13 @@ func (r AssetRepo) UpdateContractId(assetId string, contractId string) error {
 	return nil
 }
 
-func (r AssetRepo) GetPaginatedAssets(page int, limit int) ([]entity.Asset, int, error) {
-	// Calculate the offset	// Calculate offset
-	offset := (page - 1) * limit
-
-	// Query to count the total number of assets
-	countQuery := `
-        SELECT COUNT(*)
-        FROM asset;
-    `
-	var totalAssets int
-	err := r.Db.QueryRow(countQuery).Scan(&totalAssets)
+func (r AssetRepo) CreateAsset(data entity.Asset) (entity.Asset, error) {
+	res := data
+	stmt := `INSERT INTO Asset (code, issuer_id, distributor_id, name, asset_type, image ) VALUES ($1, $2, $3,$4, $5, $6) RETURNING id;`
+	err := r.Db.QueryRow(stmt, data.Code, data.Issuer.Id, data.Distributor.Id, data.Name, data.AssetType, data.Image).Scan(&res.Id)
 	if err != nil {
-		return nil, 0, fmt.Errorf("AssetRepo - GetPaginated - Count Query: %w", err)
+		return entity.Asset{}, fmt.Errorf("AssetRepo - CreateAsset - db.QueryRow: %w", err)
 	}
 
-	// Calculate total pages
-	totalPages := (totalAssets + limit - 1) / limit
-
-	// Query to fetch paginated assets
-	query := `
-        SELECT
-            a.id AS asset_id, a.name AS asset_name, a.asset_type, a.code AS code, a.image,
-            d.id AS distributor_id, d.type AS distributor_type, d.funded AS distributor_funded,
-            dk.id AS distributor_key_id, dk.public_key AS distributor_key_public_key, dk.weight AS distributor_key_weight,
-            i.id AS issuer_id, i.type AS issuer_type, i.funded AS issuer_funded,
-            ik.id AS issuer_key_id, ik.public_key AS issuer_key_public_key, ik.weight AS issuer_key_weight
-        FROM asset a
-        JOIN wallet d ON a.distributor_id = d.id
-        JOIN key dk ON d.id = dk.wallet_id
-        JOIN wallet i ON a.issuer_id = i.id
-        JOIN key ik ON i.id = ik.wallet_id
-        ORDER BY a.name
-        LIMIT $1 OFFSET $2;
-    `
-
-	rows, err := r.Db.Query(query, limit, offset)
-	if err != nil {
-		return nil, 0, fmt.Errorf("AssetRepo - GetPaginated - Query: %w", err)
-	}
-	defer rows.Close()
-
-	var assets []entity.Asset
-	for rows.Next() {
-		var asset entity.Asset
-		var distributor entity.Wallet
-		var issuer entity.Wallet
-
-		err := rows.Scan(
-			&asset.Id, &asset.Name, &asset.AssetType, &asset.Code, &asset.Image,
-			&distributor.Id, &distributor.Type, &distributor.Funded,
-			&distributor.Key.Id, &distributor.Key.PublicKey, &distributor.Key.Weight,
-			&issuer.Id, &issuer.Type, &issuer.Funded,
-			&issuer.Key.Id, &issuer.Key.PublicKey, &issuer.Key.Weight,
-		)
-		if err != nil {
-			return nil, 0, fmt.Errorf("AssetRepo - GetPaginated - row.Scan: %w", err)
-		}
-
-		asset.Distributor = distributor
-		asset.Issuer = issuer
-
-		assets = append(assets, asset)
-	}
-
-	return assets, totalPages, nil
+	return res, nil
 }
