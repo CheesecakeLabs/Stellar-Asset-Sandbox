26f0cbb745e7d6d210afa9420db0d28264c59343
Lucas Magnus <lucasmagnusdasilva@gmail.com>
2024-04-25 14:44:04 -0300

    feat: update network var

--- frontend/src/utils/constants/stellar.tsx
+++ frontend/src/utils/constants/stellar.tsx
@@ -1,5 +1,5 @@
-export const NETWORK = process.env.REACT_APP_API_URL || 'TESTNET'
+export const NETWORK = process.env.REACT_APP_NETWORK || 'TESTNET'
 
 export const HIDE_SOROBAN = process.env.REACT_APP_HIDE_SOROBAN || false
 
-export const IS_PUBLIC_NETWORK = process.env.REACT_APP_API_URL === 'PUBLIC'
+export const IS_PUBLIC_NETWORK = process.env.REACT_APP_NETWORK === 'PUBLIC'
