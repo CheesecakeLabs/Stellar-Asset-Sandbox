b08b7b7b8b21711c69310e5b5adb239372280f98
Lucas Magnus <lucasmagnusdasilva@gmail.com>
2024-06-12 14:23:06 -0300

    feat: add roles

---
+++ backend/migrations/000049_add_roles.up.sql
@@ -0,0 +1,4 @@
+INSERT INTO role (id, name, admin) VALUES (1, 'Admin', 0);
+INSERT INTO role (id, name, admin) VALUES (2, 'Asset Manager', 0);
+INSERT INTO role (id, name, admin) VALUES (3, 'Analyst', 0);
+INSERT INTO role (id, name, admin) VALUES (4, 'SuperAdmin', 1);
